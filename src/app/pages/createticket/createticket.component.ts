e9a8c9260f3c50add7e9bd5887908c02d641e092
pratiksha <pratikshajankar176@gmail.com>
2024-05-16 13:32:43 +0530

    add createticket com.

---
+++ src/app/pages/createticket/createticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './createticket.component.html',
+  styleUrl: './createticket.component.css'
+})
+export class CreateticketComponent {
+
+}
