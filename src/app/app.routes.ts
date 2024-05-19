b79aa3923f6c1228b7a0c255d86b18a78562adc5
pratiksha <pratikshajankar176@gmail.com>
2024-05-19 13:09:22 +0530

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { EmployeeComponent } from './pages/employee/employee/employee.component';
 import { DepartmentComponent } from './pages/department/department/department.component';
-import { SuperadmindashComponent } from './pages/superAdmindashboard/superadmindash/superadmindash.component';
-import { AdminemployeedashComponent } from './pages/AdminEmployee/adminemployeedash/adminemployeedash.component';
 import { DashboardComponent } from './pages/Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
