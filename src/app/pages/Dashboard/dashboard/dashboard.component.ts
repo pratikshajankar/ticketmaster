38e307e82664f01278798a1327416b9421c99b59
pratiksha <pratikshajankar176@gmail.com>
2024-05-19 12:56:59 +0530

    deleting some component

--- src/app/pages/Dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/Dashboard/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Dashboard } from '../../../core/models/Classes/Dashboard';
+import { DashboardService } from '../../../core/services/Dashboard/dashboard.service';
 
 
 
@@ -16,7 +17,7 @@ export class DashboardComponent {
 
 
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient,private dashsrv:DashboardService){}
   
     ngOnInit(): void {
     
